675e2cc05cd96d7a18e495b6d8ace42bfa2407dc
Luc Klaassen <luc@klaassen.tech>
2023-07-14 20:03:39 +0200

    Main page tweaks

--- sharerental-nuxt/components/SiteNavigation.vue
+++ sharerental-nuxt/components/SiteNavigation.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="flex justify-content-between flex-wrap bg-yellow-100 p-2">
-    <div class="flex align-items-center ml-2">
+  <div class="flex justify-content-between bg-yellow-100 p-2">
+    <div class="flex-1 align-items-center ml-2">
       <img alt="Logo" class="h-3rem" src="../assets/boels-rental-logo.jpg"/>
     </div>
-    <div class="flex align-items-center text-center font-bold text-3xl">ShareRental</div>
-    <div class="flex align-items-center">
-      <div class="bg-yellow-600 p-3 border-circle ">
-        <i class="pi pi-user"></i>
+    <div class="flex-1 align-items-center text-center font-bold text-3xl">ShareRental</div>
+    <div class="flex-1">
+      <div class="flex ">
+        <div class="absolute bg-yellow-600 p-3 mr-3 border-circle right-0">
+          <i class="pi pi-user"></i>
+        </div>
       </div>
     </div>
   </div>
+  <div class="bg-yellow-200 h-2rem"></div>
 </template>
 
 <script setup lang="ts">
--- sharerental-nuxt/pages/index.vue
+++ sharerental-nuxt/pages/index.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>
-test
+  <div class="w-full h-20rem p-3 bg-center bg-no-repeat flex flex-column justify-content-center align-items-center"
+       :style="`background-image: url(${MyBackgroundImage})`">
+    <div>
+        <span class="p-input-icon-left ">
+          <i class="pi pi-search"/>
+          <InputText v-model="searchText" placeholder="Zoek gereedschap" class="border-right-none"/>
+          <Button label="Zoeken" v-on:click="search" class="border-left-none"/>
+        </span>
+    </div>
+    <div>
+      <Button label="Ik wil mijn gereedschap verhuren" link />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import MyBackgroundImage from "@/assets/drill-background.jpg";
+
+let searchText = ref("");
+
+function search() {
+  console.log(searchText.value);
+}
 </script>
 
 <style scoped>
-
 </style>
