9cfe2b3d3de45098b73b538094b607a712ebfe40
Luc Klaassen <luc@klaassen.tech>
2023-07-21 13:13:14 +0200

    Rename file name to match documentation (fixed the header issue)

--- sharerental-nuxt/server/api/auth/[...].ts
+++ sharerental-nuxt/server/api/auth/[...].ts
@@ -1,4 +1,4 @@
-import { NuxtAuthHandler } from '#auth'
+import {NuxtAuthHandler} from '#auth'
 import OktaProvider from "next-auth/providers/okta";
 
 const runtimeConfig = useRuntimeConfig()
@@ -6,6 +6,7 @@ const runtimeConfig = useRuntimeConfig()
 export default NuxtAuthHandler({
 
     providers: [
+        // @ts-ignore Import is exported on .default during SSR, so we need to call it this way. May be fixed via Vite at some point
         OktaProvider.default({
             clientId: runtimeConfig.okta_client_id,
             clientSecret: runtimeConfig.okta_client_secret,
