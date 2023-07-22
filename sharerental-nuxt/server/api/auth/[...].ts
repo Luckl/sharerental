7ba37cd377b2b9db24929a76af2377870368a1fa
Luc Klaassen <luc@klaassen.tech>
2023-07-22 13:09:46 +0200

    Switch to firebase with password login and user creation so custom login/registration pages can be created

--- sharerental-nuxt/server/api/auth/[...].ts
+++ sharerental-nuxt/server/api/auth/[...].ts
@@ -1,16 +1,28 @@
 import {NuxtAuthHandler} from '#auth'
-import OktaProvider from "next-auth/providers/okta";
+import CredentialsProvider from "next-auth/providers/credentials";
+import {useFirebaseAuth} from "#imports";
 
-const runtimeConfig = useRuntimeConfig()
+const auth = useFirebaseAuth()
 
 export default NuxtAuthHandler({
 
     providers: [
         // @ts-ignore Import is exported on .default during SSR, so we need to call it this way. May be fixed via Vite at some point
-        OktaProvider.default({
-            clientId: runtimeConfig.okta_client_id,
-            clientSecret: runtimeConfig.okta_client_secret,
-            issuer: runtimeConfig.okta_domain
+        CredentialsProvider.default({
+            // The name to display on the sign in form (e.g. "Sign in with...")
+            name: "Credentials",
+            // `credentials` is used to generate a form on the sign in page.
+            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+            // e.g. domain, username, password, 2FA token, etc.
+            // You can pass any HTML attribute to the <input> tag through the object.
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "jsmith" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                await auth.loginUser(credentials.username.value, credentials.password.value)
+                return auth.user
+            }
         })
     ]
 })
