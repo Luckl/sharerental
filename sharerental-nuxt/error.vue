8fa90d47bb2254089cf8a898db0b2913bd09d396
Luc Klaassen <luc.klaassen@gmail.com>
2023-09-05 21:29:33 +0200

    Add error page

---
+++ sharerental-nuxt/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <Head>
+    <Title>ShareRental</Title>
+  </Head>
+  <site-navigation></site-navigation>
+  <form-page>
+    <template #content>
+      <H1>Deze pagina is nog niet af, of bestaat niet 😒 </H1>
+    </template>
+  </form-page>
+</template>
+<script setup lang="ts">
+</script>
