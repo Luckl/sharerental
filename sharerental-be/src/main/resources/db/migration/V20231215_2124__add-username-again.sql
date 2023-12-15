3ab61afc444705db1e931d6b94f4a57fae11c099
Luc Klaassen <luc.klaassen@gmail.com>
2023-12-15 21:40:38 +0100

    Fixed the flyway patch to actually work

--- sharerental-be/src/main/resources/db/migration/V20231215_2124__add-username-again.sql
+++ sharerental-be/src/main/resources/db/migration/V20231215_2124__add-username-again.sql
@@ -1,1 +1,5 @@
-alter table usr add column username varchar(255) not null default split_part(email, '@', 1);
+alter table usr add column username varchar(255);
+
+update usr set username = split_part(email, '@', 1);
+
+alter table usr alter column username set not null;
