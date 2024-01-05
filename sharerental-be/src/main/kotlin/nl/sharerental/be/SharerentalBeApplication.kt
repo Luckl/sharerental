cda5e244a57d003ce098e1758df3f6b4822997c3
Luc Klaassen <luc.klaassen@gmail.com>
2024-01-05 13:49:00 +0100

    Enable scheduling so scheduled annotation actually works

--- sharerental-be/src/main/kotlin/nl/sharerental/be/SharerentalBeApplication.kt
+++ sharerental-be/src/main/kotlin/nl/sharerental/be/SharerentalBeApplication.kt
@@ -7,10 +7,12 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.ImportRuntimeHints
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @ImportRuntimeHints(MyRuntimeHints::class)
 @ComponentScan(basePackages = ["nl.sharerental"])
+@EnableScheduling
 class SharerentalBeApplication {
 	@Bean
 	fun storage(): Storage = StorageOptions.getDefaultInstance().getService()
