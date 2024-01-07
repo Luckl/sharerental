619464c900c80a44b03d3f8caede992bf47c233a
Luc Klaassen <luc.klaassen@gmail.com>
2024-01-07 19:16:33 +0100

    Actually make spaces dashes in the string normalizer for slugs

--- sharerental-be/src/main/kotlin/nl/sharerental/be/infrastructure/StringNormalizer.kt
+++ sharerental-be/src/main/kotlin/nl/sharerental/be/infrastructure/StringNormalizer.kt
@@ -4,9 +4,9 @@ import java.text.Normalizer
 import java.util.regex.Pattern
 
 
-val pattern = Pattern.compile("[^a-zA-Z0-9]")
+val pattern = Pattern.compile("[^a-zA-Z0-9-]")
 
 private fun String.removeSpecialCharacters() = pattern.matcher(this).replaceAll("")
 
 fun String.normalizeString() =
-    Normalizer.normalize(this, Normalizer.Form.NFKD).lowercase().removeSpecialCharacters().replace(" ", "-")
+    Normalizer.normalize(this, Normalizer.Form.NFKD).lowercase().replace(" ", "-").removeSpecialCharacters()
