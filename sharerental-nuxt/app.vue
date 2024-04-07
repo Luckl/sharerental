3dfa66bc8be9260a1cf70e5b7136672bd2dc9c8f
Luc Klaassen <luc.klaassen@gmail.com>
2024-04-07 19:44:17 +0200

    Fix sizing on png for opengraph

--- sharerental-nuxt/app.vue
+++ sharerental-nuxt/app.vue
@@ -9,13 +9,13 @@ useSeoMeta({
   description: 'Huur en verhuur jouw machines en gereedschap',
   ogTitle: 'ShareRental',
   ogDescription: 'Huur en verhuur jouw machines en gereedschap',
-  ogImage: '/SR_full_green_white.png',
+  ogImage: '/SR_full_green_white_opengraph_sizing.png',
   ogImageWidth: '2580',
   ogImageHeight: '540',
   ogUrl: 'https://sharerental.nl?utm_campaign=social-redirect',
   twitterTitle: 'ShareRental',
   twitterDescription: 'Huur en verhuur jouw machines en gereedschap',
-  twitterImage: '/SR_s_green_white.png',
+  twitterImage: '/SR_full_green_white_opengraph_sizing.png',
   twitterCard: 'summary'
 })
 </script>
