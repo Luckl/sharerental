9ed3be2e488203c60d8945f6cf61d77a1d9141ee
Luc Klaassen <luc.klaassen@gmail.com>
2024-05-01 09:17:05 +0200

    new API for zip code info retrieval

---
+++ sharerental-be/src/main/kotlin/nl/sharerental/be/infrastructure/postcodeeu/PostcodeEuClient.kt
@@ -0,0 +1,58 @@
+package nl.sharerental.be.infrastructure.postcodeeu
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.server.ResponseStatusException
+
+@Service
+class PostcodeEuClient(
+    @Value("\${share-rental.postcode-eu.key}") private val key: String,
+    @Value("\${share-rental.postcode-eu.secret}") private val secret: String,
+    @Value("\${share-rental.postcode-eu.dutch-zip-lookup-url}") private val zipLookupUrl: String,
+    ) {
+
+    val slackClient = WebClient.builder()
+        .baseUrl(zipLookupUrl)
+        .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        .build()
+
+    fun getZipInfo(zip: String, houseNumber: String): ZipInfo {
+        return slackClient.get()
+            .uri("/$zip/$houseNumber")
+            .headers {
+                it.setBasicAuth(key, secret)
+            }
+            .retrieve()
+            .bodyToMono(ZipInfo::class.java)
+            .block() ?: throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "No response from postcode.eu")
+    }
+}
+
+data class ZipInfo(
+    val street: String,
+    val streetNen: String,
+    val houseNumber: Int,
+    val houseNumberAddition: String,
+    val postcode: String,
+    val city: String,
+    val cityShort: String,
+    val cityId: String,
+    val municipality: String,
+    val municipalityShort: String,
+    val municipalityId: String,
+    val province: String,
+    val rdX: Int,
+    val rdY: Int,
+    val latitude: Double,
+    val longitude: Double,
+    val bagNumberDesignationId: String,
+    val bagAddressableObjectId: String,
+    val addressType: String,
+    val purposes: List<String>,
+    val surfaceArea: Int,
+    val houseNumberAdditions: List<String>
+)
