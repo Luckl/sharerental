035d227c70222a1e5b2c6c9a34d563032a121873
Luc Klaassen <luc.klaassen@gmail.com>
2024-05-02 15:45:05 +0200

    add reflection hint for api response from postcode.eu

--- sharerental-be/src/main/kotlin/nl/sharerental/be/MyRuntimeHints.kt
+++ sharerental-be/src/main/kotlin/nl/sharerental/be/MyRuntimeHints.kt
@@ -1,16 +1,9 @@
 package nl.sharerental.be
 
-import be.woutschoovaerts.mollie.data.common.AddressRequest
-import be.woutschoovaerts.mollie.data.common.Amount
-import be.woutschoovaerts.mollie.data.common.ApplicationFee
-import be.woutschoovaerts.mollie.data.common.Locale
-import be.woutschoovaerts.mollie.data.payment.PaymentMethod
-import be.woutschoovaerts.mollie.data.payment.PaymentRequest
-import be.woutschoovaerts.mollie.data.payment.SequenceType
+import nl.sharerental.be.infrastructure.postcodeeu.ZipInfo
 import org.springframework.aot.hint.MemberCategory
 import org.springframework.aot.hint.RuntimeHints
 import org.springframework.aot.hint.RuntimeHintsRegistrar
-import org.springframework.aot.hint.TypeReference
 import java.lang.management.LockInfo
 import java.lang.management.MemoryUsage
 import java.lang.management.MonitorInfo
@@ -24,5 +17,6 @@ class MyRuntimeHints : RuntimeHintsRegistrar {
         hints.reflection().registerType(LockInfo::class.java, MemberCategory.INTROSPECT_PUBLIC_METHODS)
         hints.reflection().registerType(MonitorInfo::class.java, MemberCategory.INTROSPECT_PUBLIC_METHODS)
         hints.reflection().registerType(StackTraceElement::class.java, MemberCategory.INTROSPECT_PUBLIC_METHODS)
+        hints.reflection().registerType(ZipInfo::class.java)
     }
 }
