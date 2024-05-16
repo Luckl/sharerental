36ec403f8039f2f299a4e70073fdb46a975b73b3
Luc Klaassen <lklaassen@maqqie.nl>
2024-05-16 20:18:40 +0200

    send slack message on contact form

--- sharerental-be/src/main/kotlin/nl/sharerental/be/contactform/infrastructure/ContactFormController.kt
+++ sharerental-be/src/main/kotlin/nl/sharerental/be/contactform/infrastructure/ContactFormController.kt
@@ -2,6 +2,7 @@ package nl.sharerental.be.contactform.infrastructure
 
 import nl.sharerental.be.contactform.ContactForm
 import nl.sharerental.be.contactform.infrastructure.repository.ContactFormRepository
+import nl.sharerental.be.infrastructure.slack.SlackClient
 import nl.sharerental.contract.http.ContactFormApi
 import nl.sharerental.contract.http.model.ContactForm as HttpContactForm
 import org.slf4j.LoggerFactory
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 
 class ContactFormController(
-    private val contactFormRepository: ContactFormRepository
+    private val contactFormRepository: ContactFormRepository,
+    private val slackClient: SlackClient,
 ) : ContactFormApi {
     private val logger = LoggerFactory.getLogger(ContactFormController::class.java)
 
@@ -26,7 +28,7 @@ class ContactFormController(
             entity
         )
 
-        logger.error("Iemand heeft zijn gegevens achtergelaten: {}", entity.toString())
+        slackClient.sendSlackMessage("Iemand heeft zijn gegevens achtergelaten: ${entity.toString()}")
 
         return ResponseEntity.ok().build()
     }
