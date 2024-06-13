aef883e4bbf1983b60a72fde0c4c1c7b10664510
Luc Klaassen <luc.klaassen@gmail.com>
2024-06-13 21:38:12 +0200

    fix alignment for description

--- sharerental-nuxt/components/SrRentalItemProperty.vue
+++ sharerental-nuxt/components/SrRentalItemProperty.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="field" class="flex justify-between">
-    <span class="font-bold">{{ label }}</span>
-    <span class="text-end">{{ field }} {{ suffix }}</span>
+    <span class="font-bold mr-4">{{ label }}</span>
+    <span class="text-end ">{{ field }} {{ suffix }}</span>
   </div>
   <divider  v-if="field"></divider>
 </template>
@@ -14,5 +14,5 @@ interface Props {
 }
 
 // Define props and emitters here if needed
-const props = defineProps<Props>()
+defineProps<Props>()
 </script>
