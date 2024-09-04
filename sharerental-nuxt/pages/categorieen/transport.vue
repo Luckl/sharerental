780944ece83f08386f473e11a409a5016fa76402
Luc Klaassen <luc.klaassen@gmail.com>
2024-09-04 21:04:19 +0200

    add transport page

---
+++ sharerental-nuxt/pages/categorieen/transport.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+definePageMeta({
+  layout: 'no-header'
+})
+
+import SrCategoryPage from "~/components/SrCategoryPage.vue";
+</script>
+<template>
+  <sr-category-page category="Transport"
+                    category-description="Bij ShareRental bieden we een uitgebreid assortiment aan transportartikelen, zoals aanhangers, autotrailers en bagagewagens, ideaal voor zowel particulieren als professionals. Of je nu bezig bent met een verhuisproject, bouwmaterialen moet vervoeren of grote objecten wilt transporteren, bij ons huur je eenvoudig de juiste aanhanger voor jouw klus. Onze aanhangers zijn beschikbaar in verschillende maten en uitvoeringen, zodat je altijd het perfecte voertuig vindt voor jouw transportbehoeften."></sr-category-page>
+
+</template>
